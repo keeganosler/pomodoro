f37ff171f89fc99e23210f1213801b2a65990455
Keegan Osler <keegan.osler@vergeag.com>
2022-10-07 18:38:52 -0600

    basic stopwatch button logic

--- src/app/components/timer-main/timer-main.component.ts
+++ src/app/components/timer-main/timer-main.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { TimerService } from '../../services/timer.service';
 
 @Component({
   selector: 'app-timer-main',
   templateUrl: './timer-main.component.html',
-  styleUrls: ['./timer-main.component.scss']
+  styleUrls: ['./timer-main.component.scss'],
 })
 export class TimerMainComponent implements OnInit {
-
-  constructor() { }
+  constructor(public timerService: TimerService) {}
 
   ngOnInit(): void {
+    this.timerService.currentTime$.subscribe((t) => {
+      console.log('current time in component: ', t);
+    });
   }
 
+  onClick1() {
+    this.timerService.startTimer();
+  }
+
+  onClick2() {
+    this.timerService.stopTimer();
+  }
 }
--- src/app/services/timer.service.ts
+++ src/app/services/timer.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { Observable, scan, Subscription, take, timer } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TimerService {
+  constructor() {}
 
-  constructor() { }
+  times: { [key: string]: number } = {
+    pomodoro: 25,
+    shortBreak: 5,
+    longBreak: 15,
+  };
+
+  timerType: string = 'pomodoro' || 'shortBreak' || 'longBreak';
+
+  setTimerType(value: string) {
+    this.timerType = value;
+  }
+
+  currentTime: number = 0;
+  currentTime$: Observable<number> = new Observable();
+  timerSub: Subscription = new Subscription();
+
+  startTimer() {
+    let startTime: number =
+      this.currentTime === 0 ? this.times['pomodoro'] : this.currentTime;
+    this.currentTime$ = timer(0, 1000).pipe(
+      scan((x) => --x, startTime),
+      take(startTime)
+    );
+    this.timerSub = this.currentTime$.subscribe((t) => {
+      this.currentTime = t;
+    });
+  }
+
+  stopTimer() {
+    this.timerSub.unsubscribe();
+  }
 }
