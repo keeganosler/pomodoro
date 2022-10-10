3b7170c82ebb508617f2fbfc6b8a6c22c1eeb8d5
Keegan Osler <keegan.osler@vergeag.com>
2022-10-10 13:47:41 -0600

    settigns component: scaffolding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { SettingsComponent } from './components/settings/settings.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'pomodoro';
+
+  constructor(public dialog: MatDialog) {}
+
+  onOpenSettings() {
+    this.dialog.open(SettingsComponent, {
+      width: '600px',
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatDialogModule } from '@angular/material/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { SettingsComponent } from './components/settings/settings.component';
@@ -15,7 +17,13 @@ import { TimerTypeSwitcherComponent } from './components/timer-type-switcher/tim
     TimerMainComponent,
     SettingsComponent,
   ],
-  imports: [BrowserModule, BrowserAnimationsModule, MatButtonToggleModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonToggleModule,
+    MatDialogModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
