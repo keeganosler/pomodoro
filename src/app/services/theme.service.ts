35d351c5c0da034e29608d4080afa7d651b06d6e
Keegan Osler <keegan.osler@vergeag.com>
2022-11-07 18:12:15 -0700

    create constants and consolidate

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,5 +1,6 @@
 import { DOCUMENT } from '@angular/common';
 import { Inject, Injectable } from '@angular/core';
+import { COLOR_CLASSES, FONT_CLASSES } from '../app.contants';
 
 @Injectable({
   providedIn: 'root',
@@ -7,53 +8,41 @@ import { Inject, Injectable } from '@angular/core';
 export class ThemeService {
   constructor(@Inject(DOCUMENT) private document: Document) {}
 
-  getIsColor(color: string) {
-    return this.document.documentElement.classList.value.includes(
-      'color' + '-' + color
+  onUpdateColor(colorClassName: string) {
+    let intersection: string[] = this.classList.filter((c) =>
+      COLOR_CLASSES.includes(c)
     );
+    if (intersection.length) {
+      this.document.documentElement.classList.remove(intersection[0]);
+    }
+    this.document.documentElement.classList.add(colorClassName);
   }
 
-  getIsFont(font: string) {
-    return this.document.documentElement.classList.value.includes(
-      'font' + '-' + font
+  onUpdateFont(fontClassName: string) {
+    let intersection: string[] = this.classList.filter((c) =>
+      FONT_CLASSES.includes(c)
     );
-  }
-
-  getIsLightMode() {
-    return this.document.documentElement.classList.value.includes('light-mode');
-  }
-
-  onUpdateColor(color: string) {
-    if (this.document.documentElement.classList.value.includes('color')) {
-      let i = this.document.documentElement.classList.value.indexOf('color');
-      let classType = this.document.documentElement.classList[i];
-      this.document.documentElement.classList.remove(classType);
+    if (intersection.length) {
+      this.document.documentElement.classList.remove(intersection[0]);
     }
-    this.document.documentElement.classList.add('color' + '-' + color);
-  }
-
-  onUpdateFont(font: string) {
-    if (this.document.documentElement.classList.value.includes('font')) {
-      let i = this.document.documentElement.classList.value.indexOf('font');
-      let classType = this.document.documentElement.classList[i];
-      this.document.documentElement.classList.remove(classType);
-    }
-    this.document.documentElement.classList.add('font' + '-' + font);
+    this.document.documentElement.classList.add(fontClassName);
   }
 
   onUpdateLightMode(lightMode: boolean) {
     if (lightMode) {
-      if (this.document.documentElement.classList.value.includes('dark-mode')) {
+      if (this.classList.includes('dark-mode')) {
         this.document.documentElement.classList.remove('dark-mode');
       }
       this.document.documentElement.classList.add('light-mode');
     } else {
-      if (
-        this.document.documentElement.classList.value.includes('light-mode')
-      ) {
+      if (this.classList.includes('light-mode')) {
         this.document.documentElement.classList.remove('light-mode');
       }
       this.document.documentElement.classList.add('dark-mode');
     }
+  }
+
+  get classList(): string[] {
+    return this.document.documentElement.classList.value.split(' ');
   }
 }
